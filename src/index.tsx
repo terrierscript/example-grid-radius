1727b161c85e304b26e9343b44aa054970af1b49
terrierscript <13282103+terrierscript@users.noreply.github.com>
2019-09-01 17:56:03 +0900

    add

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,45 @@
 import React from "react"
 import { render } from "react-dom"
+import styled from "styled-components"
 
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 60px);
+  grid-auto-rows: 60px;
+`
+
+const Item = styled.div`
+  vertical-align: center;
+  text-align: center;
+`
+const Aitem = styled(Item)`
+  border-top: black solid 1px;
+`
+const Ritem = styled(Item)`
+  /* border-top: black solid 1px; */
+  border-left: black solid 1px;
+  border-bottom: black solid 1px;
+  border-top: black solid 1px;
+  border-radius: 50% 0 0 50%;
+`
+const Litem = styled(Item)`
+  /* border-top: black solid 1px; */
+  border-right: black solid 1px;
+  border-bottom: black solid 1px;
+  border-top: black solid 1px;
+  border-radius: 0 50% 50% 0;
+`
 const App = () => {
-  return <div>hello</div>
+  return (
+    <Grid>
+      <Ritem>r</Ritem>
+      <Aitem>a</Aitem>
+      <Aitem>a</Aitem>
+      <Aitem>a</Aitem>
+      <Aitem>a</Aitem>
+      <Litem>a</Litem>
+    </Grid>
+  )
 }
 
 render(<App />, document.querySelector("#container"))
